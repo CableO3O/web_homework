d06cd8b9e0a454b25b36622562b378507e796f89
cable <cable870526@gmail.com>
2024-01-10 14:40:47 +0800

    修改資料功能完成

--- api/sell_change.php
+++ api/sell_change.php
@@ -11,6 +11,9 @@ if (!empty($_FILES['img']['tmp_name'])) {
     if (!empty($_POST['user_img']) && file_exists($folderPath . $_POST['user_img'])) {
         unlink($folderPath . $_POST['user_img']);
     }
+}else {
+    // 沒有上傳新檔案時，保留舊的檔案名稱
+    $filename = $_POST['user_img'];
 }
 $good=[
     'id'=>$_POST['id'],
