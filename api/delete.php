771520bcffc10de35708f425135b50bf13a6ee89
cable <cable870526@gmail.com>
2024-01-10 15:39:22 +0800

    下架功能完成(含刪除照片)

--- api/delete.php
+++ api/delete.php
@@ -1,3 +1,7 @@
 <?php include_once "db.php";
-
+$folderPath = "../imgs/";
+$good=$Shop->find($_POST['id']);
+if (file_exists($folderPath . $good['img'])) {
+    unlink($folderPath . $good['img']);
+}
 $Shop->del($_POST['id']);
