d8d5d582c7cef7c5306f4f98c57e94a9c0faacfa
cable <cable870526@gmail.com>
2024-01-17 15:44:35 +0800

    修復購物車功能

--- api/add_shopcar.php
+++ api/add_shopcar.php
@@ -1,12 +1,14 @@
 <?php
 include_once "db.php";
-if (!isset($_POST['id'])) {
+if (isset($_POST['id'])) {
     $Shopcar->save($_POST);
-}else{
-    if (isset($_SESSION['id'])) {
+} else {
+    if (isset($_SESSION['id']) && $_SESSION['user']=='admin') {
+        echo 2;
+    } else if (isset($_SESSION['id'])) {
         $Shopcar->save($_POST);
         echo 1;
-    }else{
+    } else {
         echo 0;
     }
 }
--- front/good.php
+++ front/good.php
@@ -19,48 +19,48 @@ $good = $Shop->find($_GET['id']);
 </style>
 <div class="container-fluid" style="height:30%; background-color: lightblue;">
     <div class="row  justify-content-center">
-            <div class="col-md-6 col-12" >
-                <img style="width: 69%;" src="./imgs/<?= $good['img']; ?>" alt="">
+        <div class="col-md-6 col-12">
+            <img style="width: 69%;" src="./imgs/<?= $good['img']; ?>" alt="">
+        </div>
+        <div class="col-md-6 col-12" style="text-align: left;">
+            <input type="hidden" name="user_id" id="user_id" value="<?= (isset($_SESSION['id'])) ? $_SESSION['id'] : ''; ?>">
+            <input type="hidden" name="good_id" id="good_id" value="<?= $good['id']; ?>">
+            <div class="good mt-5">
+                <label class="" for="name">商品名稱:</label>
+                <input readonly class="text form-control" type="text" name="name" id="name" value="<?= $good['name']; ?>">
             </div>
-            <div class="col-md-6 col-12" style="text-align: left;">
-                <input type="hidden" name="user_id" id="user_id" value="<?= (isset($_SESSION['id'])) ? $_SESSION['id'] : ''; ?>">
-                <input type="hidden" name="good_id" id="good_id" value="<?= $good['id']; ?>">
-                <div class="good mt-5">
-                    <label class="" for="name">商品名稱:</label>
-                    <input readonly class="text form-control" type="text" name="name" id="name" value="<?= $good['name']; ?>">
-                </div>
-                <div class="good mt-5">
-                    <label class="" for="price">商品價格:</label>
-                    <input readonly style="color: red;" class="text form-control" type="text" name="price" id="price" value="<?= $good['price']; ?>">
-                </div>
-                <div class="good mt-5">
-                    <label class="" for="name">商品說明:</label>
-                    <input readonly class="text form-control" type="text" name="text" id="text" value="<?= $good['text']; ?>">
-                </div>
-                <div class="good mt-5">
-                    <label class="" for="count">數量:&nbsp;</label>
-                    <input style="width: 30%;" class="form-control" type="number" name="count" id="count" value="1">
-                </div>
-                <?php
-                if (isset($_SESSION['id'])) {
-                    $check = $Shopcar->count("where `user_id`='{$_SESSION['id']}' and `good_id`='{$_GET['id']}'");
-                    if ($check == 0) {
-                ?>
-                        <button class="btn btn-primary btn-lg mt-5" onclick="add()">加入購物車</button>
-                    <?php
-                    } else {
-                    ?>
-                        <button class="btn btn-danger btn-lg mt-5" onclick="del()">從購物車刪除</button>
-                    <?php
-                    }
-                } else {
-                    ?>
+            <div class="good mt-5">
+                <label class="" for="price">商品價格:</label>
+                <input readonly style="color: red;" class="text form-control" type="text" name="price" id="price" value="<?= $good['price']; ?>">
+            </div>
+            <div class="good mt-5">
+                <label class="" for="name">商品說明:</label>
+                <input readonly class="text form-control" type="text" name="text" id="text" value="<?= $good['text']; ?>">
+            </div>
+            <div class="good mt-5">
+                <label class="" for="count">數量:&nbsp;</label>
+                <input style="width: 30%;" class="form-control" type="number" name="count" id="count" value="1">
+            </div>
+            <?php
+            if (isset($_SESSION['id'])) {
+                $check = $Shopcar->count("where `user_id`='{$_SESSION['id']}' and `good_id`='{$_GET['id']}'");
+                if ($check == 0) {
+            ?>
                     <button class="btn btn-primary btn-lg mt-5" onclick="add()">加入購物車</button>
                 <?php
-                }
+                } else {
                 ?>
-            </div>
-            
+                    <button class="btn btn-danger btn-lg mt-5" onclick="del()">從購物車刪除</button>
+                <?php
+                }
+            } else {
+                ?>
+                <button class="btn btn-primary btn-lg mt-5" onclick="add()">加入購物車</button>
+            <?php
+            }
+            ?>
+        </div>
+
     </div>
 </div>
 <script>
@@ -75,7 +75,10 @@ $good = $Shop->find($_GET['id']);
             pay: 0
         }
         $.post("./api/add_shopcar.php", good, (res) => {
-            if (parseInt(res) == 1) {
+            if (parseInt(res) == 2) {
+                alert("此帳號為管理員無法購物");
+                location.href = "./index.php?do=main";
+            } else if (parseInt(res) == 1) {
                 alert("已加入購物車");
                 location.href = "./index.php?do=main";
             } else {
